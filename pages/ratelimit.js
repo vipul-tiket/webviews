1fecfe9845e81bed492318f6e837ec3465c2e110
vipul sharma <vipul.sharma@tiket.com>
2023-03-03 18:26:50 +0530

    fixes

--- pages/ratelimit.js
+++ pages/ratelimit.js
@@ -28,7 +28,7 @@ export default function BackOrClose() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Tiket Masuk Konser Musik, Paket Wisata, Hiburan - tiket.com</title>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;600&display=swap" rel="stylesheet" />
       </Head>
 
